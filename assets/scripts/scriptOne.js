5d218e764e045b9fe0dd34466f4b537e91d39804
james cruickshank <jaemsnrg@gmail.com>
2018-10-25 09:56:41 +0100

    Initial Commit

---
+++ assets/scripts/scriptOne.js
@@ -0,0 +1,16 @@
+var Man = {
+  firstName: "Koby",
+  lastName: "Kimber",
+  age: 32,
+  eyeColor: "maroon"
+};
+
+function startScript() {
+  console.log(Man.firstName);
+  console.log('yo');
+}
+
+startScript()
+
+
+
